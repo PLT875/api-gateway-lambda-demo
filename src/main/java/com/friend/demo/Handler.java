f2fb8824797be07f0d4d3b175483010aef90aa5a
peter.tran <peter.tran@sainsburys.co.uk>
2020-11-28 21:10:39 +0000

    Initial commit

---
+++ src/main/java/com/friend/demo/Handler.java
@@ -0,0 +1,10 @@
+package com.friend.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class Handler {
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("Input: " + input);
+        return "Hello World - " + input;
+    }
+}
